e64bd02c585c093229bc14d8b1b2c33314c69b2e
Junfeng Quan <quan.ju@northeastern.edu>
2023-03-22 21:01:04 -0400

    123

---
+++ infoLab5/src/model/System.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author yulon
+ */
+public class System {
+    ArrayList<DoctorDirectory> doctorDirs;
+    ArrayList<PatientDirectory> patientDirs;
+    ArrayList<PeopleDirectory> peopleDirs;
+    ArrayList<HospitalDirectory> hospitalDirs;
+    ArrayList<Community> communities;
+    String cities[];
+    
+}
